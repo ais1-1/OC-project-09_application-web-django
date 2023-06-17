031f4aed5cf0c2ecf1c2793775e07f78fbb5450d
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-06-17 15:45:30 +0200

    add home and authentication pages

--- litreviewbooks/settings.py
+++ litreviewbooks/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'litreviewbooks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'authentication.User'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = LOGIN_URL
--- litreviewbooks/urls.py
+++ litreviewbooks/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+import authentication.views
+import bookreview.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', authentication.views.signup_page, name='signup'),
+    path('home/', bookreview.views.home, name='home'),
 ]
